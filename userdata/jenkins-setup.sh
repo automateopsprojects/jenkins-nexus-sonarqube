34980ddf142a64d0fb25f18936ce3dbf29813f27
automateopsprojects <120359592+automateopsprojects@users.noreply.github.com>
2023-07-19 14:28:37 +0530

    Update jenkins-setup.sh

--- userdata/jenkins-setup.sh
+++ userdata/jenkins-setup.sh
@@ -6,9 +6,5 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
 sudo apt-get update -y
 sudo apt-get install jenkins -y
 sudo apt update -y
-sudo apt-get install openjdk-17-jdk -y
-java -version
-openjdk version "17.0.7" 2023-04-18
-OpenJDK Runtime Environment (build 17.0.7+7-Debian-1deb11u1)
-OpenJDK 64-Bit Server VM (build 17.0.7+7-Debian-1deb11u1, mixed mode, sharing)
+sudo apt install openjdk-11* -y
 
